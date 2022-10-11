018bf7998c06877dc1bec316082bd341ac1ee866
josias Germán De Lima <josiasdelima080@gmail.com>
2022-10-11 15:04:57 -0300

    Implementacion en el Front

--- FrontEnd-Dinamico/src/app/app.module.ts
+++ FrontEnd-Dinamico/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { CircleProgressComponent } from './componentes/circle-progress/circle-progress.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    
+    BannerComponent,
+    AcercaDeComponent,
+    
+    EducacionComponent,
+    ExperienciaLaboralComponent,
+    CircleProgressComponent,
+    ProyectosComponent,
+    FooterComponent,
+          
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
